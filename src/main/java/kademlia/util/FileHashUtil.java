252513e66d039d17d23d03334e528688515f73c2
卡拉马佐夫 <973459080@qq.com>
2021-01-21 18:58:52 +0800

    file init

--- src/main/java/kademlia/util/FileHashUtil.java
+++ src/main/java/kademlia/util/FileHashUtil.java
@@ -13,27 +13,6 @@ import java.security.MessageDigest;
  */
 public class FileHashUtil {
 
-    private static final char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5',
-            '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-
-    /**
-     * Takes the raw bytes from the digest and formats them correct.
-     *
-     * @param bytes the raw bytes from the digest.
-     * @return the formatted bytes.
-     */
-    private static String getFormattedText(byte[] bytes) {
-        int len = bytes.length;
-        StringBuilder buf = new StringBuilder(len * 2);
-        // 把密文转换成十六进制的字符串形式
-        for (int i = 0; i < bytes.length; i++) {
-            byte aByte = bytes[i];
-            buf.append(HEX_DIGITS[(aByte >> 4) & 0x0f]);
-            buf.append(HEX_DIGITS[aByte & 0x0f]);
-        }
-        return buf.toString();
-    }
-
     public static byte[] sha1Hash(byte[] bytes) {
         if (bytes == null) {
             return null;
@@ -47,31 +26,12 @@ public class FileHashUtil {
         }
     }
 
-
     public static String shaHashCode(String filePath) throws FileNotFoundException {
         FileInputStream fis = new FileInputStream(filePath);
         BigInteger bigInt = new BigInteger(1, shaHashCode(fis));//1代表绝对值
         return bigInt.toString(16);//转换为16进制
     }
 
-    /**
-     * 保证文件的MD5值为32位
-     *
-     * @param filePath 文件路径
-     * @return
-     * @throws FileNotFoundException
-     */
-    public static String md5HashCode32(String filePath) throws FileNotFoundException {
-        FileInputStream fis = new FileInputStream(filePath);
-        return md5HashCode32(fis);
-    }
-
-    /**
-     * java获取文件的md5值
-     *
-     * @param fis 输入流
-     * @return
-     */
     public static byte[] shaHashCode(InputStream fis) {
         byte[] bytes = null;
         try {
@@ -90,48 +50,5 @@ public class FileHashUtil {
         }
         return bytes;
     }
-
-    /**
-     * java计算文件32位md5值
-     *
-     * @param fis 输入流
-     * @return
-     */
-    public static String md5HashCode32(InputStream fis) {
-        try {
-            //拿到一个MD5转换器,如果想使用SHA-1或SHA-256，则传入SHA-1,SHA-256
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            //分多次将一个文件读入，对于大型文件而言，比较推荐这种方式，占用内存比较少。
-            byte[] buffer = new byte[1024];
-            int length = -1;
-            while ((length = fis.read(buffer, 0, 1024)) != -1) {
-                md.update(buffer, 0, length);
-            }
-            fis.close();
-
-            //转换并返回包含16个元素字节数组,返回数值范围为-128到127
-            byte[] md5Bytes = md.digest();
-            StringBuffer hexValue = new StringBuffer();
-            for (int i = 0; i < md5Bytes.length; i++) {
-                int val = ((int) md5Bytes[i]) & 0xff;//解释参见最下方
-                if (val < 16) {
-                    /**
-                     * 如果小于16，那么val值的16进制形式必然为一位，
-                     * 因为十进制0,1...9,10,11,12,13,14,15 对应的 16进制为 0,1...9,a,b,c,d,e,f;
-                     * 此处高位补0。
-                     */
-                    hexValue.append("0");
-                }
-                //这里借助了Integer类的方法实现16进制的转换
-                hexValue.append(Integer.toHexString(val));
-            }
-            return hexValue.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-
 
 }
